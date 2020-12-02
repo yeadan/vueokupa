68f6a8549f9d62123a0b66e2800a1d4e3155711e
Sergio Bosch Vega <sergio.bosch@gmail.com>
2020-12-02 20:37:25 +0100

    Add files via upload

--- src/components/pie.vue
+++ src/components/pie.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <footer class="footer py-3 bg-dark text-white">
-      <div class="container text-center">
+      <div class="text-center">
         <small>Copyright &copy; <strong>2020</strong> Sergio Bosch Vega </small>
         <a class="" target="_blank" href="https://github.com/yeadan/vueokupa">
-				<img align="right" src="../../public/github_logo.png" style="height:25px;width:auto" />
+				<img align="right" src="../../public/github_logo.png" style="margin-right:50px;height:25px;width:auto" />
 			</a>
       </div>
     </footer>
